1ce758fc25a9163fd7167c3077f8e0003259034b
Vlad Maschenko <1995talant@gmail.com>
2023-03-26 13:36:54 +0300

    test husky 13:36

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,3 +1,10 @@
-export {};
+import Input from "../shared/Input/Input";
+import { Button } from "../shared/Button/Button";
 
-var a = 123;
+var a = "123";
+
+function d() {
+  var f = "123";
+}
+
+export {};
